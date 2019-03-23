1198f74b9d4e9b8cb027280196037cbd221480e2
ImFlog <garcia.florian.perso@gmail.com>
2019-03-23 16:19:33 +0100

    Update versions : java 11, Spring, Spring cloud ...

--- gateway/src/main/java/com/github/imflog/gateway/security/SecurityConfiguration.java
+++ gateway/src/main/java/com/github/imflog/gateway/security/SecurityConfiguration.java
@@ -2,19 +2,22 @@ package com.github.imflog.gateway.security;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
@@ -24,7 +27,13 @@ import org.springframework.security.web.authentication.session.NullAuthenticated
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@KeycloakConfiguration
+// Bug in keycloak, see : https://issues.jboss.org/browse/KEYCLOAK-8725
+// @KeycloakConfiguration
+@Configuration
+@ComponentScan(
+        basePackageClasses = KeycloakSecurityComponents.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.keycloak.adapters.springsecurity.management.HttpSessionManager"))
+@EnableWebSecurity
 public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     @Value("${access-control-max-age}")
