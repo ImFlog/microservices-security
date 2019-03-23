1e03d2a6ed9dbbcb0b7910a32574cb0984872cb3
ImFlog <garcia.florian.perso@gmail.com>
2019-03-23 16:58:14 +0100

    Add start script

--- start-keycloak.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d --name=keycloak \
-    -e KEYCLOAK_USER=admin \
-    -e KEYCLOAK_PASSWORD=admin \
-    -e KEYCLOAK_IMPORT=/import/realm-export.json \
-    -v $(pwd)/realm-export.json:/import/realm-export.json \
-    -p 8080:8080 \
-    jboss/keycloak
---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if ! type "docker" > /dev/null; then
+    echo "ERROR: docker is required please install it first: https://github.com/localstack/localstack"
+    exit 1
+fi
+
+keycloak=$(docker run -d --name=keycloak \
+    -e KEYCLOAK_USER=admin \
+    -e KEYCLOAK_PASSWORD=admin \
+    -e KEYCLOAK_IMPORT=/import/realm-export.json \
+    -v $(pwd)/realm-export.json:/import/realm-export.json \
+    -p 8080:8080 \
+    jboss/keycloak)
+docker exec ${keycloak} keycloak/bin/add-user-keycloak.sh -r keycloak-demo -u user -p test1234 --roles yes_we_can
+docker restart ${keycloak}
+
+function cleanup {
+    echo "Stopping Containers..."
+    docker rm -f ${keycloak}
+    kill ${gateway}
+    kill ${resource}
+}
+
+trap cleanup EXIT
+
+# start gateway
+./gradlew gateway:bootRun &
+gateway=$!
+# start resource
+./gradlew resource:bootRun &
+resource=$!
+# start frontend
+
+cd front && npm install && npm start
