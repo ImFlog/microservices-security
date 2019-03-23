0582b3c8edd5187108ad6eb5ae271264b08acd97
Vivien Maleze <vmaleze@gmail.com>
2019-03-23 15:37:09 +0100

    Edit environment config

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000/secured',
+    apiUrl: 'http://localhost:8081/secured',
     keycloak: {
         url: 'http://localhost:8080/auth',
         realm: 'keycloak-demo',
